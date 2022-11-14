ceb3aed5a8042ea81a316131c5426c72daaa15db
JoonasPitk <46161@edu.raseko.fi>
2022-11-14 14:22:33 +0200

    Bring Head.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import Head from "next/head";
 import Layout from "@/components/layout"
 
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
     <Layout>
+        <Head>
+            <title>Kauppalista</title>
+            <meta
+                name="description"
+                content="Kauppalistan hallintaohjelma"
+            />
+            <link rel="icon" href="/kauppalista.png" />
+        </Head>
         <Component {...pageProps} />
     </Layout>
     );
