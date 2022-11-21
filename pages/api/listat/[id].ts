a7515d105be5df11e7527a92d0fe33a5db40e6b2
JoonasPitk <46161@edu.raseko.fi>
2022-11-21 14:37:21 +0200

    Handle lists' GET and errors.

---
+++ pages/api/listat/[id].ts
@@ -0,0 +1,33 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {getDatabase} from '@/utils/db';
+import {ErrorResponse} from '@/utils/ErrorResponse';
+import {ShopList} from '@/utils/types';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ShopList | ErrorResponse>
+) {
+    if (req.method == "GET") {
+        const {query} = req;
+        const db = await getDatabase();
+        const {id} = query;
+        const idNumber: number | null = tryParseInt(<string>id);
+        if (idNumber === null) {
+            res.status(400).json({
+                error: `The "id" must be integer`,
+                errorCode: "bad-request",
+            });
+            return;
+        }
+        const shopLists = await db.getShopList(idNumber);
+        res.status(200).json(shopLists)
+    }
+}
+
+function tryParseInt(value:string | null | undefined): number | null {
+    try {
+        const result = parseInt(value ?? "") ?? null;
+        return isNaN(result) ? null : result;
+    } catch (e) {}
+    return null;
+}
--- utils/db.ts
+++ utils/db.ts
@@ -1,16 +1,29 @@
-import * as sqlite from 'sqlite';
-import sqlite3 from 'sqlite3';
+#!/usr/bin/env -S npx ts-node --project node.tsconfig.json
+
+import * as sqlite from "sqlite";
+import sqlite3 from "sqlite3";
+import {ShopList, ShopListItem} from "./types";
 
 export async function getDatabase() {
     const db: sqlite.Database = await sqlite.open({
-        filename: 'database.db',
+        filename: "database.db",
         driver: sqlite3.Database,
     });
 
     await db.exec(`
     CREATE TABLE IF NOT EXISTS shoplist (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR(100)
+        name VARCHAR(100) NOT NULL,
+        created_at DATETIME NOT NULL
+    )`);
+
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS shoplist_item (
+        list_id INTEGER NOT NULL,
+        sequence INTEGER NOT NULL,
+        done BOOLEAN NOT NULL DEFAULT 0,
+        item VARCHAR(200) NOT NULL,
+        PRIMARY KEY (list_id, sequence)
     )`);
 
     return new Database(db);
@@ -23,8 +36,79 @@ export class Database {
         this.db = db;
     }
 
-    async createShopList(name: string) {
-        await this.db.run('INSERT INTO shoplist (name) VALUES (?)', name);
-        //const result = await db.all('SELECT * FROM testi');
+    async createShopList(name: string): Promise<number> {
+        const result = await this.db.run(
+            `INSERT INTO shoplist (name, created_at)
+            VALUES (?, datetime('now'))`,
+            name
+        );
+        return result.lastID!;
+    }
+
+    async addItemToList(listId: number, item: string): Promise<void> {
+        const result = await this.db.get(
+            `SELECT MAX(sequence) AS maxSeq FROM shoplist_item
+            WHERE list_id = ?`,
+            listId
+        );
+        console.log(result);
+        const maxSeq: number = result.maxSeq ?? 0;
+
+        this.db.run(
+            `INSERT INTO shoplist_item (list_id, sequence, item)
+            VALUES (?, ?, ?)`,
+            listId,
+            maxSeq + 1,
+            item
+        );
+    }
+
+    async getShopLists(): Promise<ShopList[]> {
+        const rows = await this.db.all("SELECT * FROM shoplist");
+        return rows.map((row) => ({
+            id: row.id,
+            name: row.name,
+            createdAt: new Date(row.created_at + "Z"),
+        }));
+    }
+
+    async getShopList(listId: number): Promise<ShopList> {
+        const row = await this.db.get(
+            'SELECT * FROM shoplist WHERE id=?',
+            listId
+        );
+        return {
+            id: row.id,
+            name: row.name,
+            createdAt: new Date(row.created_at + 'Z'),
+        };
+    }
+
+    async getListItems(listId: number): Promise<ShopListItem[]> {
+        const rows = await this.db.all(
+            "SELECT * FROM shoplist_item WHERE list_id=?",
+            listId
+        );
+        return rows.map((row) => ({
+            listId: row.list_id,
+            sequence: row.sequence,
+            text: row.item,
+            done: row.done ? true : false,
+        }));
     }
 }
+
+async function kokeile() {
+    const db = await getDatabase();
+    const listaId = await db.createShopList("Testilista");
+    console.log(listaId);
+    await db.addItemToList(listaId, "eka rivi");
+    await db.addItemToList(listaId, "toka rivi");
+    await db.addItemToList(listaId, "kolmas rivi");
+    const listat = await db.getShopLists();
+    console.log(`${listat[0].createdAt}`);
+    const ekanListanIteemit = await db.getListItems(listat[0].id);
+    console.log(ekanListanIteemit);
+}
+
+kokeile();
