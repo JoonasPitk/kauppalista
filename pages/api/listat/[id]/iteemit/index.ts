e0475037cf043b13a9fc2af7da74bb077b2e6eb6
JoonasPitk <46161@edu.raseko.fi>
2022-11-21 14:46:24 +0200

    Create new types, and move files.

--- pages/api/listat/[id]/iteemit/index.ts
+++ pages/api/listat/[id]/iteemit/index.ts
@@ -1,28 +1,27 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+
 import {getDatabase} from '@/utils/db';
 import {ErrorResponse} from '@/utils/ErrorResponse';
-import {ShopList} from '@/utils/types';
+import {ShopListItem} from '@/utils/types';
 import {tryParseInt} from '@/utils/numbers';
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<ShopList | ErrorResponse>
+    res: NextApiResponse<ShopListItem[] | ErrorResponse>
 ) {
     if (req.method == "GET") {
         const {query} = req;
         const db = await getDatabase();
-        const {id} = query;
-        const idNumber: number | null = tryParseInt(<string>id);
-        if (idNumber === null) {
+        const {id: listIdString} = query;
+        const listId: number | null = tryParseInt(<string>listIdString);
+        if (listId === null) {
             res.status(400).json({
                 error: `The "id" must be integer`,
                 errorCode: "bad-request",
             });
             return;
         }
-        const shopLists = await db.getShopList(idNumber);
-        res.status(200).json(shopLists)
+        const items = await db.getListItems(listId);
+        res.status(200).json(items);
     }
 }
-
-
