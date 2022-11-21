9ab8ddf60e6be295325d2d3e69cbb80f62b2b90e
JoonasPitk <46161@edu.raseko.fi>
2022-11-21 14:58:59 +0200

    Idk, lol.

--- pages/api/listat/[id]/iteemit/index.ts
+++ pages/api/listat/[id]/iteemit/index.ts
@@ -9,19 +9,29 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ShopListItem[] | ErrorResponse>
 ) {
+    const {query} = req;
+    const db = await getDatabase();
+    const {id: listIdString} = query;
+    const listId: number | null = tryParseInt(<string>listIdString);
+    if (listId === null) {
+        res.status(400).json({
+            error: `The "id" must be integer`,
+            errorCode: "bad-request",
+        });
+        return;
+    }
     if (req.method == "GET") {
-        const {query} = req;
-        const db = await getDatabase();
-        const {id: listIdString} = query;
-        const listId: number | null = tryParseInt(<string>listIdString);
-        if (listId === null) {
-            res.status(400).json({
-                error: `The "id" must be integer`,
-                errorCode: "bad-request",
-            });
-            return;
-        }
         const items = await db.getListItems(listId);
         res.status(200).json(items);
+    } else if (req.method == "POST") {
+        const {text} = req.body;
+        if (!text) {
+            res.status(400).json({
+                error: `The "text" is missing`,
+                errorCode: "bad-request",
+            })
+            return;
+        }
+        const sequence = await db.addItemToList(listId, text);
     }
 }
